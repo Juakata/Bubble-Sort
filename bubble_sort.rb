a82a7fa1801a1b46825c78df0745b69099dee285
Juakata <ha.uzquiano@hotmail.com>
2019-08-27 14:37:24 -0600

    fixing more issues

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,20 +2,21 @@
 
 def print_arr(arr)
   arr.each do |v|
-    print v.to_s + " "
+    print "#{v} "
   end
   print "\n"
 end
+
 def bubble_sort(arr)
   sort = false
-  while !sort
-    sort=true
+  until sort
+    sort = true
     for i in 0...arr.length-1
-      if arr[i]>arr[i+1]
+      if arr[i] > arr[i+1]
         aux = arr[i]
         arr[i] = arr[i+1]
-        arr[i+1]=aux
-        sort=false
+        arr[i+1] = aux
+        sort = false
       end
     end
   end
@@ -27,19 +28,20 @@ bubble_sort([0,-6,10,2000,5000])
 def bubble_sort_by(arr)
   sort = false
   while !sort
-    sort=true
+    sort = true
     for i in 0...arr.length-1
       yield(arr[i], arr[i+1])
-      if arr[i].length>arr[i+1].length
+      if arr[i].length > arr[i+1].length
         aux = arr[i]
         arr[i] = arr[i+1]
-        arr[i+1]=aux
-        sort=false
+        arr[i+1] = aux
+        sort = false
       end
     end
   end
   print_arr(arr)
 end
+
 bubble_sort_by(["hi","hello","hey"]) do |left,right|
   puts left.length - right.length
 end

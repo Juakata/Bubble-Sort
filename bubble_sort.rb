f710a446bfe46e4251e140bd346dab7de4823620
dev1980 <brham1980@gmail.com>
2019-08-28 02:10:10 +0545

    bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,7 +1,9 @@
 def printArr(arr)
   arr.each do |v|
     print v.to_s + " "
+
   end
+  print "\n"
 end
 def bubble_sort(arr)
   sort = false
@@ -20,3 +22,23 @@ def bubble_sort(arr)
 end
 
 bubble_sort([0,-6,10,2000,5000])
+
+def bubble_sort_by(arr)
+  sort = false
+  while !sort
+    sort=true
+    for i in 0...arr.length-1
+      yield(arr[i], arr[i+1])
+      if arr[i].length>arr[i+1].length
+        aux = arr[i]
+        arr[i] = arr[i+1]
+        arr[i+1]=aux
+        sort=false
+      end
+    end
+  end
+  printArr(arr)
+end
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+puts left.length - right.length
+end
